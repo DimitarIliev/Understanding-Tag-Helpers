566b58959c09cb99e4da65c219cf2bcb4c372f61
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-07-24 22:59:25 +0200

    Built-in Tag Helpers.

--- Cities/Controllers/HomeController.cs
+++ Cities/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Cities.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Cities.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
         private IRepository repository;
 
@@ -18,9 +19,21 @@ namespace Cities.Controllers
 
         public ViewResult Index() => View(repository.Cities);
 
-        public ViewResult Create() => View();
+        public ViewResult Edit()
+        {
+            ViewBag.Countries = new SelectList(repository.Cities.Select(x => x.Country).Distinct());
+            return View("Create", repository.Cities.FirstOrDefault());
+        }
+        
+
+        public ViewResult Create()
+        {
+            ViewBag.Countries = new SelectList(repository.Cities.Select(x => x.Country).Distinct());
+            return View();
+        }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(City city)
         {
             repository.AddCity(city);
---
+++ Cities/Infrastructure/TagHelpers/SelectOptionTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Cities.Models;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Cities.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("select", Attributes = "model-for")]
+    public class SelectOptionTagHelper: TagHelper
+    {
+        private IRepository _repository;
+
+        public SelectOptionTagHelper(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ModelExpression ModelFor { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.Append((await output.GetChildContentAsync(false)).GetContent());
+
+            object selected;
+            context.Items.TryGetValue(typeof(SelectTagHelper), out selected);
+            IEnumerable<string> selectedValues = (selected as IEnumerable<string>) ?? Enumerable.Empty<string>();
+
+            PropertyInfo property = typeof(City).GetTypeInfo().GetDeclaredProperty(ModelFor.Name);
+
+            foreach (var country in _repository.Cities.Select(x => property.GetValue(x)).Distinct())
+            {
+                if (selectedValues.Any(x => x.ToString().Equals(country.ToString(), StringComparison.OrdinalIgnoreCase)))
+                    output.Content.AppendHtml($"<option selected>{country}</option>");
+                else
+                    output.Content.AppendHtml($"<option>{country}</option>");
+            }
+        }
+    }
+}
